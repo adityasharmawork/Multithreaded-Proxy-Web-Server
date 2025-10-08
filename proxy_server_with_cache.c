fcb7480751754ac0134228d53e14812503973d1f
Aditya Sharma <adityasharma2005123@gmail.com>
2025-10-09 00:14:46 +0530

    Created thread logic for mac clients

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include<pthread.h>
 
+#define MAX_CLIENTS 10
 typedef struct cache_element cache_element;
+
 
 struct cache_element { // This is basically a list of all cache elements (cache_element_list), a linked list
     char* data; // The data we received as response when we fetched the server for a specific request
@@ -20,3 +23,7 @@ void remove_cache_element();
 int port_number = 8080;
 int proxy_socketId;
 
+// We will create new separate threads for each new client request, to make our application multithreaded (Handles multiple concurrent requests)
+// As soon as any client sends a request to our proxy server, we will create a new thread for that and open a new socket for handling those client requests
+
+pthread_t tid[MAX_CLIENTS];
